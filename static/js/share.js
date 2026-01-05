83dd53eddc2ce2c7f4cc3c727554257e9b80add6
syjang <jsy_0189@naver.com>
2026-01-05 22:51:57 +0900

    진짜 마지막이라고 해

--- static/js/share.js
+++ static/js/share.js
@@ -3,7 +3,7 @@ function kakaoShare(){
         Kakao.init('c7242b41eaad5d31a1ff6c0de0f82f32');
     }
     Kakao.Share.sendCustom({
-        templateId: 1363341,
+        templateId: 127662,
         templateArgs: {
             title: 'Songs For You!',
             description: '당신에게 꼭 맞는 곡을 PICK!',
